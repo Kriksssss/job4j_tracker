c17d5f1faa51e4c1cdcc50bd6b3acf5f1f4fcf3f
Kriksssss <nabibeg53@gmail.com>
2022-10-25 14:20:24 +0300

    1.3. Вызов метода с аргументами. [#166086 #354762]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -12,9 +12,9 @@ public class Jukebox {
     }
 
     public static void main(String[] args) {
-        Jukebox james = new Jukebox();
-        james.music(1);
-        james.music(2);
-        james.music(3);
+        Jukebox jame = new Jukebox();
+        jame.music(1);
+        jame.music(2);
+        jame.music(3);
     }
 }
