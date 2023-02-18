9135f3231f1f412a2b98048dd915744afce0d70b
Kriksssss <nabibeg53@gmail.com>
2023-02-18 07:12:18 +0300

    9. Тестирование. Подготовка данных. [#182960 #392491]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -37,6 +37,6 @@ public class StartUITest {
         String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertThat(deleted == null);
+        assertThat(deleted).isNull();
     }
 }
