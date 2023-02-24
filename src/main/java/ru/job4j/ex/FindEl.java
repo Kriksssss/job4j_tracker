fda63ff90511369e3cd2c8d6b3365a7018866c5b
Kriksssss <nabibeg53@gmail.com>
2023-02-24 07:45:48 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,29 @@
+package ru.job4j.ex;
+
+import java.util.Objects;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (Objects.equals(value[i], key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("нет такого индекса в массиве");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] k = new String[] {"lll", "sss", "ooo"};
+        try {
+            System.out.println(indexOf(k, "sdsd"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
