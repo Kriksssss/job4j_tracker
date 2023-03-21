46ecec18041c1c019a6db79747ef5be65b36c7b4
Kriksssss <nabibeg53@gmail.com>
2023-03-21 20:00:45 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,9 +8,9 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
-            } else break;
+            if (element.getPriority() > task.getPriority()) {
+                break;
+            } else index++;
         }
         this.tasks.add(index, task);
     }
