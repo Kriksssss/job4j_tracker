52d29209c87e030c67ae213d58d1a356427da2e2
Kriksssss <nabibeg53@gmail.com>
2023-04-08 22:42:31 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("makashmakash555@gmail.com", "Nabi Dzhebiev");
+        map.put("nabibeg53@gmail.com", "Nabi Dzhebiev");
 
         for (String key : map.keySet()) {
             String value = map.get(key);
