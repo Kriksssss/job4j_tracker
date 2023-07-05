dd2a20a15ebeceb93ba59528816445fa4ec8536b
Kriksssss <nabibeg53@gmail.com>
2023-07-06 00:17:21 +0600

    2.3. Встроенные функциональные интерфейсы. [#249209  [#249209]]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -7,9 +7,9 @@ import java.util.function.Predicate;
 public class SearchFolder {
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
         List<Folder> rsl = new ArrayList<>();
-        for (Folder f : list) {
-            if(pred.test(f)) {
-                rsl.add(f);
+        for (Folder folder : list) {
+            if (pred.test(folder)) {
+                rsl.add(folder);
             }
         }
         return rsl;
